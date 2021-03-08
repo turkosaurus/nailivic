a83a92d4711d401925da8aa3d97b28b80dc83786
Travis Turk <travisturk@gmail.com>
2021-03-08 16:46:11 -0600

    navbar and basic routes added

--- application.py
+++ application.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Octochaos!'
+def dashboard():
+    return render_template('index.html')
+
+@app.route('/parts')
+def parts():
+    return 'hello parts'
+
+@app.route('/items')
+def items():
+    return 'hello items'
+
+@app.route('/shipping')
+def shipping():
+    return 'hello shipping'
+
+@app.route('/register')
+def register():
+    return 'hello register'
+
+@app.route('/login')
+def login():
+    return 'hello login'
