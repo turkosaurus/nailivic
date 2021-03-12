987c5d3e773fed0fa7a145f6ad66882619c3a85a
Travis Turk <travisturk@gmail.com>
2021-03-12 16:39:04 -0600

    added loteria data classes and types

--- application.py
+++ application.py
@@ -1,8 +1,42 @@
 from flask import Flask, redirect, render_template, url_for
+from dataclasses import dataclass
+
+
 app = Flask(__name__)
+
+
+
+# Set type variables
+# Loteria
+colors = ['black', 'red', 'turquoise', 'yellow', 'green', 'purple']
+sizes = ['s', 'm', 'l']
+loterias = {
+    'La Dama': ['Frida', 'Frida Flowers'],
+    'La Sirena': ['Mermaid Body', 'Mermaid Hair', 'Mermaid Tail'],
+    'La Mano': ['Hand', 'Hand Swirls'],
+    'La Bota': ['Boot', 'Boot Swirls', 'Boot Flames'],
+    'El Corazon': ['Heart', 'Heart Swirls'],
+    'El Musico': ['Guitar', 'Guitar Hands'],
+    'La Estrella': ['Star', 'Star Swirls'],
+    'El Pulpo': ['Octoups', 'Octopus Swirls', 'Octopus Tentacles'],
+    'La Rosa': ['Rose', 'Rose Swirls', 'Rose Leaves'],
+    'La Calavera': ['Skull', 'Skull Flames', 'Skull Swirls'],
+    'El Poder': ['Fist', 'Fist Swirls', 'Fist Wrist']
+}
+
+@dataclass
+class loteria:
+    a: 
 
 @app.route('/')
 def dashboard():
+    # print(loteria)
+    for key in loterias:
+        # i = 0
+        print(f"{key}:")
+        tmp = loterias[key]
+        print(tmp)
+
     return render_template('index.html')
 
 @app.route('/parts')
