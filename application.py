6d8ea33f3b044dda2939bdfb7266a303c73d2168
Travis Turk <travisturk@gmail.com>
2021-03-14 15:21:50 -0500

    setup db url env variable; cs50, psycopg2

--- application.py
+++ application.py
@@ -2,23 +2,38 @@ import os
 import requests
 import urllib.parse
 
+from cs50 import SQL
 from flask import Flask, redirect, render_template, request, session, url_for
 from flask_session import Session
+from tempfile import mkdtemp
 from functools import wraps
 from dataclasses import dataclass
 
-
-
-
-
-
-
-
-
+###### CONFIGURATION ######
+# Initialize Flask App Ojbect
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure Heroku Postgres database
+db = SQL(os.getenv('DATABASE_URL'))
+
+###### DATA STRUCTURES ######
 # Set type variables
 # Loteria
 colors = ['black', 'red', 'turquoise', 'yellow', 'green', 'purple']
@@ -31,7 +46,7 @@ loterias = {
     'El Corazon': ['Heart', 'Heart Swirls'],
     'El Musico': ['Guitar', 'Guitar Hands'],
     'La Estrella': ['Star', 'Star Swirls'],
-    'El Pulpo': ['Octoups', 'Octopus Swirls', 'Octopus Tentacles'],
+    'El Pulpo': ['Octopus', 'Octopus Swirls', 'Octopus Tentacles'],
     'La Rosa': ['Rose', 'Rose Swirls', 'Rose Leaves'],
     'La Calavera': ['Skull', 'Skull Flames', 'Skull Swirls'],
     'El Poder': ['Fist', 'Fist Swirls', 'Fist Wrist']
@@ -43,6 +58,20 @@ class loteria:
     a: str
     b: str
     c: str
+
+# Helper Functions
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
