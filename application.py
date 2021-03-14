b6d1d90d933b496f16c8a9a7336c0b68248982b4
Travis Turk <travisturk@gmail.com>
2021-03-14 15:32:26 -0500

    /login, /register, error.html

--- application.py
+++ application.py
@@ -59,21 +59,6 @@ class loteria:
     b: str
     c: str
 
-# Helper Functions
-def login_required(f):
-    """
-    Decorate routes to require login.
-
-    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
-    """
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
-            return redirect("/login")
-        return f(*args, **kwargs)
-    return decorated_function
-
-
 @app.route('/')
 def dashboard():
     # print(loteria)
@@ -101,14 +86,97 @@ def projections():
 def shipping():
     return render_template('shipping.html')
 
-@app.route('/register')
-def register():
-    return "todo"
-    # return render_template('register.html')
 
-@app.route('/login')
+
+###### ADMINSTRATIVE ######
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # Serve registration page
+    if request.method == 'GET':
+        return render_template("register.html")
+
+    # Process submitted form responses on POST
+    else:
+
+        # Error Checking
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", errcode=403, errmsg="Username required.")
+
+        # Ensure password was submitted
+        if not request.form.get("password"):
+            return render_template("error.html", errcode=403, errmsg="Password required.")
+
+        # Ensure password and password confirmation match
+        if request.form.get("password") != request.form.get("passwordconfirm"):
+            return render_template("error.html", errcode=403, errmsg="Passwords must match.")
+
+        # Ensure minimum password length
+        if len(request.form.get("password")) < 8:
+            return render_template("error.html", errcode=403, errmsg="Password must be at least 8 characters.")
+
+        # Store the hashed username and password
+        username = request.form.get("username")
+        hashedpass = generate_password_hash(request.form.get("password"))
+
+        # Check if username is already taken
+        if not db.execute("SELECT username FROM users WHERE username LIKE (?)", username):
+
+            # Add the username
+            db.execute("INSERT INTO users (username, hash) VALUES (:username, :hashedpass)",
+                        username=username, hashedpass=hashedpass)
+            return redirect("/")
+
+        else:
+            return render_template("error.html", errcode=403, errmsg="Username invalid or already taken.")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return 'hello login'
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return render_template("error.html", errcode=400, errmsg="Username required.")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return render_template("error.html", errcode=400, errmsg="Password required.")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists
+        if len(rows) != 1:
+            return render_template("register.html", errmsg="Username not found.")
+
+        # Ensure username exists and password is correct
+        if not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return render_template("error.html", errcode=403, errmsg="Incorrect password.")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Store login timestamp
+        db.execute("INSERT INTO activity (user_id, action) VALUES (:user_id, :action)", user_id=session["user_id"], action="login")
+
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 
 def login_required(f):
